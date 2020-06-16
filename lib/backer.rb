ba0aea1bebe1fb5da0400abfebedb2ed565a751e
Sajid Anjum <sajid1760@gmail.com>
2020-06-16 23:53:13 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+
+class Backer
+  
+attr_accessor :name, :backed_projects
+
+def initialize(name)
+  @name = name
+  @backed_projects = []
+end
+
+def back_project(project = "hello")
+  @backed_projects << project
+  project.add_backer(self)
+end 
+
+end
+
+
+
