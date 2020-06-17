8307bd23907857b403d6cac7534e54e057e71d75
Sajid Anjum <sajid1760@gmail.com>
2020-06-17 00:05:43 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,7 +10,7 @@ end
 
 def back_project(project)
   existingproject = @backed_projects.find { |oldproject| oldproject == project }
-  if existingproject = nil then
+  if existingproject == nil then
     @backed_projects << project
     #project.add_backer(self)
   end
